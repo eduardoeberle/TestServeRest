7639d196c3482a16671d9d951dc2542446a7fe9e
unknown <eduardoeberle@hotmail.com>
2025-06-06 09:59:01 -0300

    reutilizar metodo

--- cypress/e2e/FRONT/Login.cy.js
+++ cypress/e2e/FRONT/Login.cy.js
@@ -1,22 +1,18 @@
 describe('Efetuar Login', () => {
   beforeEach(() => {
-    const url = Cypress.env('frontUrl') || 'https://front.serverest.dev/login'
+    const url = Cypress.env('frontUrl') || 'https://front.serverest.dev'
     cy.visit(url);
   });
 
   it('Efetuar login', () => {
-    cy.Digitar('[data-testid="email"]', 'fulano@qa.com');
-    cy.Digitar('[data-testid="senha"]', 'teste');
-    cy.Clicar('[data-testid="entrar"]');
+    cy.LoginWeb('fulano@qa.com', 'teste');
     
     cy.url().should('include', '/home');
     cy.Verificar('.jumbotron', 'Fulano');
   });
 
   it('Login inválido', () => {
-    cy.Digitar('[data-testid="email"]', 'fulano@qa.com');
-    cy.Digitar('[data-testid="senha"]', 'teste2');
-    cy.Clicar('[data-testid="entrar"]');
+    cy.LoginWeb('fulano@qa.com', 'teste2');
 
     cy.Verificar('[class="alert alert-secondary alert-dismissible"]', 'Email e/ou senha inválidos');
   });
--- cypress/e2e/FRONT/Produtos.cy.js
+++ cypress/e2e/FRONT/Produtos.cy.js
@@ -5,14 +5,9 @@ describe('Listar Produtos', () => {
   });
 
   it('Efetuar login e Listar produtos', () => {
-    cy.Digitar('[data-testid="email"]', 'fulano@qa.com');
-    cy.Digitar('[data-testid="senha"]', 'teste');
-    cy.Clicar('[data-testid="entrar"]');
-    
-    cy.url().should('include', '/home');
-    cy.Verificar('.jumbotron', 'Fulano');
+    cy.LoginWeb('fulano@qa.com', 'teste', 'Fulano');
 
     cy.Clicar('[data-testid="listarProdutos"]');
-    cy.Verificar('.jumbotron', 'Produto QA');
+    cy.Verificar('.jumbotron', 'Bespoke Granite Gloves');
   });
 });
